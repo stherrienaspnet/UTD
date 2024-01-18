8d08d0ec795f0af2562131ccd714b755f3500160
Stephane Therrien <stherrienaspnet@gmail.com>
2024-01-18 12:18:26 -0500

    Fixed issue with UTD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule,  } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -15,16 +15,17 @@ export class AppComponent implements OnInit  {
 
   constructor() { }
   
-  public loadScript(url: string) {
+  public loadScript(baseUrl: string, fileUrl: string) {
     let node = document.createElement('script');
-    node.src = url;
+    node.src = `${baseUrl}${fileUrl}`;
+    node.setAttribute('relative-base-path', baseUrl);
     node.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(node);
+    document.getElementsByTagName('body')[0].appendChild(node);
 }
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.loadScript("https://cdn.jsdelivr.net/gh/MTESSDev/utd-webcomponents@1.17.0/dist/js/utd-webcomponents.min.js");
+      this.loadScript("https://cdn.jsdelivr.net/gh/MTESSDev/utd-webcomponents@1.17.0/dist", "/js/utd-webcomponents.min.js");
     }, 1);
   }
 }
