5d738c7f86c37b93a80661a274e7cbdc8697a1b7
Stephane Therrien <stherrienaspnet@gmail.com>
2024-01-18 09:16:54 -0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { APP_INITIALIZER, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { CommonModule,  } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'minimal';
+
+  constructor() { }
+  
+  public loadScript(url: string) {
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loadScript("https://cdn.jsdelivr.net/gh/MTESSDev/utd-webcomponents@1.17.0/dist/js/utd-webcomponents.min.js");
+    }, 1);
+  }
 }
